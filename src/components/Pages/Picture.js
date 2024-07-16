db2a0a82c582a4ba93b0d7ea267c67db144dec3e
annup76779 <annup76779@gmail.com>
2024-07-17 02:13:40 +0530

    Picture has photo galary working in responsive manner

--- src/components/Pages/Picture.js
+++ src/components/Pages/Picture.js
@@ -1,18 +1,27 @@
 import React from "react";
 import Photo from "../Photo";
+import test_image from "../../assets/test_image.jpg";
+import lamp from "../../assets/lamp.png";
+import test_image2 from "../../assets/test_image2.png";
 
 function Picture() {
     return (
         <>
             <div className="container-fluid">
                 <div className="column-container">
-                    <Photo />
+                    <Photo src={test_image} title={"Test Image"} />
+                    <Photo src={test_image2} title={"Test Image 2"} />
+                    <Photo src={lamp} title={"Lamp"} />
                 </div>
                 <div className="column-container">
-                    <Photo />
+                    <Photo src={lamp} title={"Lamp"} />
+                    <Photo src={test_image} title={"Test Image"} />
+                    <Photo src={test_image2} title={"Test Image 2"} />
                 </div>
                 <div className="column-container">
-                    <Photo />
+                    <Photo src={test_image2} title={"Test Image 2"} />
+                    <Photo src={lamp} title={"Lamp"} />
+                    <Photo src={test_image} title={"Test Image"} />
                 </div>
             </div>
         </>
